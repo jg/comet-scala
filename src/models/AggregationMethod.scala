0cd90b58ad75eec7cdfbdf318d0372bd8ed2f464
Juliusz Gonera <juliusz.gonera@gmail.com>
2013-07-07 22:00:33 +0200

    Auto aggregation sources can make use of Mechanize agent

--- src/models/AggregationMethod.scala
+++ src/models/AggregationMethod.scala
@@ -6,13 +6,13 @@ trait ManualAggregation extends AggregationMethod
 /** For sources in which we can somehow pick the issue we want to aggregate
  *  IssueId can be for example a Date (for DateSources) or an AppearanceNumber (for NumberSources)
  */
-trait FullAggregation extends AggregationMethod with MechanizeAgent {
+trait FullAggregation extends AggregationMethod with Mechanize.Agent {
   type IssueId
   def aggregate(id: IssueId): Option[AggregationData]
 }
 
 /** For some sources we can only grab what is available at the moment of aggregation */
-trait LimitedAggregation extends AggregationMethod with MechanizeAgent {
+trait LimitedAggregation extends AggregationMethod with Mechanize.Agent {
   type IssueId
 
   def aggregateAvailable: Option[AggregationData]
