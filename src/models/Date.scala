d5a2f9f7d9d7a68a562855b2ae4f235e9d2a4d3b
Juliusz Gonera <juliusz.gonera@gmail.com>
2013-07-07 19:30:08 +0200

    Use JodaTime for Date class

--- src/models/Date.scala
+++ src/models/Date.scala
@@ -1,10 +1,10 @@
-import java.util.{Date => javaDate}
+import org.joda.time.DateTime
 
 object Date {
-  // def now: Date = new Date()
+  def now: Date = new Date(new DateTime())
 }
-class Date(d: javaDate) {
-  def strftime(fmt: String): String = {
-    ""
-  }
+
+class Date(d: DateTime) {
+  /** Available formats http://joda-time.sourceforge.net/apidocs/org/joda/time/format/DateTimeFormat.html */
+  def strftime(fmt: String): String = d.toString(fmt)
 }
